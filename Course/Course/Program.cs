b285c8473255aedf620b3e60d6086d056653d309
marcosmanfre <marcosmanfre@hotmail.com>
2023-07-25 10:02:18 -0300

    Initial commit

---
+++ Course/Course/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';
+            byte n1 = 127;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria";
+            object obj1 = "Alex";
+            int teste1 = int.MinValue;
+            int idade = 31;
+            double saldo = 10.35784;
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(teste1);
+
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual {2:F2} reais", nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual {saldo:F2} reais");
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais" );
+        }   
+    }
+}
---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroPojeto
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
