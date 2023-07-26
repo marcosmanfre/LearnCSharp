9d1de1f9ddd9480eb0682e537ef04e396b2b168a
marcosmanfre <marcosmanfre@hotmail.com>
2023-07-26 18:33:38 -0300

    dice

--- DiceGame/DiceGame/Program.cs
+++ DiceGame/DiceGame/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DiceGame
+{ 
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int playerRandomNum;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+
+                playerRandomNum = random.Next(1, 7);
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a  " + playerRandomNum);
+            }
+            Console.ReadLine();
+
+        }
+    }
+        
+ }
