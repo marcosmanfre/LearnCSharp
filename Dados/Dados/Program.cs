ffbeef35ab5d91a988809f78a0927000ae829093
marcosmanfre <marcosmanfre@hotmail.com>
2023-07-31 10:22:18 -0300

    program

--- Dados/Dados/Program.cs
+++ Dados/Dados/Program.cs
@@ -11,7 +11,13 @@ namespace Dados
             string y = Console.ReadLine();
             string z = Console.ReadLine();
 
-            Console.WriteLine("Você digitou: " + frase);
+            string s = Console.ReadLine();
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
             Console.WriteLine(frase);
             Console.WriteLine(x);
             Console.WriteLine(y);
